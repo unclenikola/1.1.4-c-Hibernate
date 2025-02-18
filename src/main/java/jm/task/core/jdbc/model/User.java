fde83f8661f452bc225b2a08f9ed2a89b0edab26
unclenikola <9107066065@mail.ru>
2025-02-18 09:35:58 +0300

    раскомминтировал servisJDBC и добавил к аннотации @Table имя тадлицы

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -3,7 +3,7 @@ package jm.task.core.jdbc.model;
 import javax.persistence.*;
 
 @Entity
-@Table
+@Table (name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
